9138f03d9ed45edeb95ac6d6656bd2dae931ca80
Karin Marjieh <karinmarjieh@hotmail.com>
2023-07-20 15:14:48 +0300

    added array methods

--- 3 - array methods/index.js
+++ 3 - array methods/index.js
@@ -37,3 +37,78 @@ console.log(
 // console.log(
 //   calculateTotalHealth(players)
 // );
+
+// ["Hero - Level 1", "Warrior - Level 2"]
+
+const playerNamesAndLevels = (players) => {
+  return players.map((player) => {
+    return player.name + " - Level " + player.level;
+  });
+};
+console.log(
+  playerNamesAndLevels([
+    { name: "Hero", level: 1 },
+    { name: "Warrior", level: 2 },
+  ])
+);
+console.log(playerNamesAndLevels(players));
+
+const updatePlayerHealth = (players, newHealth, playerName) => {
+  players.map((player) => {
+    if (player.name === playerName) {
+      player.health = newHealth;
+    }
+  });
+  return players;
+};
+
+console.log(
+  updatePlayerHealth(
+    [
+      { name: "Hero", health: 100 },
+      { name: "Warrior", health: 120 },
+    ],
+    (newHealth = 150),
+    (playerName = "Hero")
+  )
+);
+// console.log(updatePlayerHealth(players, (newHealth = 150), (playerName = "Hero")));
+// console.log(players)
+
+const filterPlayersByLocation = (players, location) => {
+  return players.filter((player) => {
+    return player.location === location;
+  });
+};
+console.log(
+  filterPlayersByLocation(
+    [{ location: "forest" }, { location: "mountain" }, { location: "castle" }],
+    (location = "forest")
+  )
+);
+// console.log(filterPlayersByLocation(players, (location = "forest")));
+
+const getAveragePlayerLevel = (players) => {
+  return parseInt(
+    players.reduce((accumulator, player) => {
+      return accumulator + player.level;
+    }, 0) / players.length
+  );
+};
+// console.log(getAveragePlayerLevel(players));
+console.log(getAveragePlayerLevel([{ level: 1 }, { level: 2 }, { level: 3 }]));
+
+const sortPlayersbyHealth = (players) => {
+  return players.sort((a, b) => b.health - a.health);
+};
+console.log(
+  sortPlayersbyHealth([{ health: 100 }, { health: 120 }, { health: 80 }])
+);
+
+const retrievePlayerInventories = (players) => {
+  return players.map((player) => {
+    return player.inventory;
+  });
+};
+
+console.log(retrievePlayerInventories(players));
